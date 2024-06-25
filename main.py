9c2bbdea3202ea487318e6f0160b9de2bdb29ab1
MrDoritos <ianmoore322@gmail.com>
2024-06-24 23:25:46 -0700

    empty message

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import sys
 import time
 
+import plotly.express as px
+import plotly.graph_objects as go
+
 fin = sys.stdin
 fout = sys.stdout
 
@@ -16,11 +19,35 @@ data = []
 
 fin.readline()
 
+def is_float(element: any) -> bool:
+    #If you expect None to be passed:
+    if element is None: 
+        return False
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
+
 while fin.readable():
     line = fin.readline()
     if not line:
         break
-    data.append([float(x.strip()) for x in line.split(',') if x.strip() != ''])
+    _l = []
+
+    #remove marker from split
+
+    for column in line.split(',')[1:]:
+        text = column.strip()
+        if text == '':
+            continue
+        if is_float(text):
+            _l.append(float(text))
+        else:
+            _l.append(0.0)
+    data.append(_l)
+
+    #data.append([float(x.strip()) for x in line.split(',') if x.strip() != ''])
 
 def format_data(data:list):
     out_data = []
@@ -29,6 +56,17 @@ def format_data(data:list):
         for j in range(len(data[i])):
             if fields[j] != 'STAMP':
                 _l.append(data[i][j])
+        out_data.append(_l)
+    return out_data
+
+def format_pairs(data:list):
+    out_data = []
+    for i in range(len(data)):
+        _l = []
+        print(data[i])
+        for j in range(len(data[i])):
+            if fields[j] == 'STAMP':
+                _l.append([data[i][j], data[i][j+1]])
         out_data.append(_l)
     return out_data
 
@@ -48,12 +86,42 @@ def normalize_data(in_data:list):
     for i in range(len(in_data)):
         _l = []
         for j in range(len(in_data[i])):
-            _l.append((in_data[i][j] - min_vals[j]) / (max_vals[j] - min_vals[j]))
+            _l.append((in_data[i][j] - min_vals[j] + 0.00000001) / (max_vals[j] - min_vals[j] + 0.00000001))
         out_data.append(_l)
 
     return out_data
 
+def normalize_pairs(in_data:list):
+    #to_normalize = [pair[1] for sublist in in_data for pair in sublist]
+    to_normalize = []
+
+    for sublist in in_data:
+        _l = []
+        for pair in sublist:
+            _l.append(pair[1])
+        to_normalize.append(_l)
+
+    normalized = normalize_data(to_normalize)
+
+    out_data = []
+    
+    #for i in range(len(in_data)):
+    #    _l = []
+    #    for j in range(len(in_data[i])):
+    #        _l.append([in_data[i][j][0], normalized[i][j]])
+
+    for row_i,row in enumerate(in_data):
+        _l = []
+        for pair_i,pair in enumerate(row):
+            _l.append([pair[0], normalized[row_i][pair_i]])
+        out_data.append(_l)
+
+    return out_data
+    
+normalized_pairs = format_pairs(data)#normalize_pairs(format_pairs(data))
 normalized_data = normalize_data(format_data(data))
+
+formatted_fields = [x for x in fields if x != 'STAMP']
 
 def print_fields(fields):
     for i in range(len(fields)):
@@ -63,12 +131,20 @@ def print_fields(fields):
     print()
 
 try:
-    print_fields([x for x in fields if x != 'STAMP'])
+    #print_fields([x for x in fields if x != 'STAMP'])
 
     for i in range(len(normalized_data)):
-        print_fields(normalized_data[i])
+        pass#print_fields(normalized_data[i])
 
     fout.close()
 except Exception as e:
     #print(e)
     pass
+
+fig = go.Figure()
+
+for i in range(len(formatted_fields)):
+    #fig.add_trace(go.Scatter(x=[j for j in range(len(normalized_data))], y=[_y[i] for _y in normalized_data], name=formatted_fields[i]))
+    fig.add_trace(go.Scatter(x=[_x[i][0] for _x in normalized_pairs], y=[_y[i][1] for _y in normalized_pairs], name=formatted_fields[i]))
+
+fig.show()
